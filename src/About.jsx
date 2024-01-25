7074f3ce058e7c3a778b80e572aa7ef28e55bf41
kmSidharthM <kmsidharthmurali@gmail.com>
2024-01-26 04:08:19 +0530

    few modifications in about page

--- src/About.jsx
+++ src/About.jsx
@@ -8,14 +8,17 @@ import java from './assets/java.svg'
 import proImage from './assets/pro2.png'
 import location from './assets/location.png'
 import education from './assets/education.png'
+import github from './assets/github.svg'
+import linkedin from './assets/linkedin.svg'
 const About = () => {
   return ( 
     <div className="app-about">
       <div className="left-content">
         <div className="basic-intro">
+        <p style={{fontSize : 40, fontWeight : 200, marginBottom : 10}}>About Me....</p>
           <p>I'm a 4th year undergrad in Computer Science from Government Engineering College Thrissur with a strong background in programming and a passion for solving problems. Driven to succeed as a software engineer, while pursuing continuous improvement.</p>
-          <p>I worked as a Software Engineer Intern at Ayu Health (May - Aug 2023).</p>
-          <p>Completed my 12th from Kendriya Vidyalaya Keltron Nagar, Kannur in Computer Science.</p>
+          <p>I have worked as a Software Engineer Intern at Ayu Health (May - Aug 2023).</p>
+          <p>I Completed my 12th from Kendriya Vidyalaya Keltron Nagar, Kannur in Computer Science.</p>
         </div>
         <p style={{fontSize : 40, fontWeight : 200, marginBottom : 10}}>Skills</p>
         <div className="skills">
@@ -26,6 +29,11 @@ const About = () => {
           <img src={css} alt="css-logo" width={50} />
           <img src={js} alt="js-logo" width={50} />
           <img src={rt} alt="react-logo" width={50} />
+        </div>
+        <p style={{fontSize : 40, fontWeight : 200, marginBottom : 10}}>Profiles</p>
+        <div className="profiles">
+          <p className='personal-info'><span><img src={github} width={35} /></span><a href="https://github.com/kmSidharthM" target='_blank'>kmSidharthM</a></p>
+          <p className='personal-info'><span><img src={linkedin} width={35} /></span><a href="https://www.linkedin.com/in/kmsidharthmurali/" target='_blank'>kmsidharthmurali</a></p>
         </div>
       </div>
       <div className="right-content">
